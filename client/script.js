dc946e90168370ca8fb61546a788634c97a91f5b
Just Nass <127904963+Nasser-Alzaid@users.noreply.github.com>
2024-07-31 19:16:58 +0300

    fixed the page and implementing some hs

--- client/script.js
+++ client/script.js
@@ -89,6 +89,36 @@ function loader (element){
 
       loader(massegeDiv);
 
+      // fetch data from server -> bot's response
+
+      const response = await fetch('http://localhost:5000', {
+
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt : data.get('prompt')
+
+      })
+    
+    })
+
+    clearInterval(loadIntervel);
+    messageDivDiv.innerHTML = '';
+
+    if(response.ok){
+      const data = await response.json();
+      const parsredData = data.bot.trim();
+
+      typeText(messageDiv, parsredData)
+    } else{
+      const err = await response.text();
+
+      massegeDiv.innerHTML = "Something went wrong";
+
+      alert(err);
+    }
 
     }
 
